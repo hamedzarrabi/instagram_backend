c8599ac37c4b2b9b3185a4c847c9b3e2990625c7
Hamed Zarrabi <hamed.zarrabi87@gmail.com>
2023-02-22 10:43:53 +0330

    Commit 2 - Constant

---
+++ src/main/java/com/instagram/enumeration/Role.java
@@ -0,0 +1,17 @@
+package com.instagram.enumeration;
+
+import static com.instagram.constans.Authority.*;
+public enum Role {
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES);
+
+    private String[] authorities;
+
+    Role(String... authorities) {
+        this.authorities = authorities;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+}
---
+++ src/main/java/com/instagram/http/HttpResponse.java
@@ -0,0 +1,20 @@
+package com.instagram.http;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Iran/Tehran")
+    private Date timeStamp;
+    private int httpStatusCode; //200, 201, 400, 500
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+}
